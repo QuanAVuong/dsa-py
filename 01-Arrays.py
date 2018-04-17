f8f61e4a7e7777c5e7fdf83184b20992b7f9761a
Quan <QuanAVuong@users.noreply.github.com>
2018-04-16 23:35:35 -0400

    loop 1

--- 01-Arrays.py
+++ 01-Arrays.py
@@ -1,6 +1,9 @@
 lItems = ["string", 3984, [True, False], {"float": 1.4, "fraction": 1/5}]
 
-print(lItems[2])
+print("3 =>", lItems[2])
 
 lItems[0] = "Hello World"
-print(lItems[0])
+print("6 =>", lItems[0])
+
+for item in lItems:
+  print("9 =>", item)
