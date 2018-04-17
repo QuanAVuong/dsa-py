06e608d836ff0c81006f828280695091d33585e5
Quan <QuanAVuong@users.noreply.github.com>
2018-04-16 23:54:54 -0400

    Range literals: https://www.python.org/dev/peps/pep-0204/#the-proposed-solution

--- 01-Arrays.py
+++ 01-Arrays.py
@@ -10,4 +10,14 @@ for item in lItems:
 
 
 for i in range(len(lItems)):
-  print("13:", lItems[i])
+  print("13 =>", lItems[i])
+
+# 4.6.1. Common Sequence Operations
+# https://docs.python.org/3/library/stdtypes.html
+print("16 =>", lItems[0:2])
+print("17 =>", lItems[:2])
+print("18 =>", lItems[:])
+print("19 =>", lItems[:-1])
+print("20 =>", lItems[-2:])
+print("21 =>", lItems[::2])
+print("21 =>", lItems[::-1])
