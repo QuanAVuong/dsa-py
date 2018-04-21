cc05c2106c723501bfdec0ad2b57e8790d80a6aa
Quan <QuanAVuong@users.noreply.github.com>
2018-04-21 15:55:57 -0400

    remove: when removal node is both head and tail

--- 02-LinkedLists.py
+++ 02-LinkedLists.py
@@ -54,20 +54,26 @@ class LinkedList(object):
       currentNode                = currentNode.nextNode
 
 
-	def remove(self, data):
+	def remove(self, data): 
 	
-		if self.head is None:
+		if self.head is None:  
 			return
 			
-		self.size = self.size - 1
+		self.size           = self.size - 1
 		
     # Setting up currentNode / previousNode at the beginning
-    currentNode = self.head
-    previousNode = None
+    currentNode         = self.head
+    previousNode        = None
 
     print(f"\nSearching for node containing `{data}` to remove:")
-
-    while currentNode.data != data:
+    while currentNode.data  != data: 
       print(f"currentNode.data {currentNode.data} != {data}")
-      previousNode = currentNode
-      currentNode = currentNode.nextNode
+      previousNode           = currentNode
+      currentNode            = currentNode.nextNode
+    print(f"FOUND! currentNode.data {currentNode.data} == {data} ! Removing `{currentNode.data}`")
+
+    # Removal control flow based on found node's location
+    if previousNode is None and currentNode.nextNode is None:
+			print(f"The only remaining node: {currentNode.data} is both a head and a tail")
+			print(f"Poiting this list's only head to None will clear it.")
+			self.head = currentNode.nextNode
