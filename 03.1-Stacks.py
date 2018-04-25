c6b8e3df5a129a33c26717481375645b2f308644
Quan <QuanAVuong@users.noreply.github.com>
2018-04-25 05:10:56 -0400

    03.1 - update sizeStack() to only return its size

--- 03.1-Stacks.py
+++ 03.1-Stacks.py
@@ -9,7 +9,7 @@ class Stack:
     # [print(f"| {self.stack[-i-1]} |") if i < len(self.stack)-1 else print(f"|_{self.stack[-i-1]}_|") for i, v in enumerate(self.stack)]
     for i, v in enumerate(self.stack):
       if i == 0 and operation == "push": 
-        print("|___|" if len(self.stack) == 1 else "|   |", f"<-- {self.stack[-i-1]}")
+        print("|___|" if self.sizeStack() == 1 else "|   |", f"<-- {self.stack[-i-1]}")
       elif i == 0 and operation == "pop": 
         print(f"|   | --> {self.stack[-1]}")
       elif i < len(self.stack)-1:
@@ -24,7 +24,7 @@ class Stack:
       # switcher.get(i)
 
   def sizeStack(self):
-    return f"Stack's size: {len(self.stack)}"
+    return len(self.stack)
   
   def push(self, data):
     self.stack.append(data)
@@ -49,10 +49,10 @@ stack1.push(2)
 stack1.push(3)
 stack1.push(4)
 stack1.push(5)
-print(stack1.sizeStack())
+print("Stack's size:", stack1.sizeStack())
 
 stack1.pop()
 stack1.pop()
-print(stack1.sizeStack())
+print("Stack's size:", stack1.sizeStack())
 # print("Peek:", stack1.peek())
 # print(stack1.sizeStack())
