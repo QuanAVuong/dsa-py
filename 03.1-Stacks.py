575d6422328a920b5a101fdf674406d28b5f704a
Quan <QuanAVuong@users.noreply.github.com>
2018-04-25 03:24:04 -0400

    03.1 - showStack(): visualizing the stack vertically

--- 03.1-Stacks.py
+++ 03.1-Stacks.py
@@ -5,6 +5,9 @@ class Stack:
   def isEmpty(self):
     return self.stack == []
 
+  def showStack(self):
+    [print(f"| {self.stack[-i-1]} |") if i < len(self.stack)-1 else print(f"|_{self.stack[-i-1]}_|") for i, v in enumerate(self.stack)]
+
   def push(self, data):
     self.stack.append(data)
 
@@ -15,22 +18,21 @@ class Stack:
       
     return self.stack.pop()
 
-  
   def peek(self):
     return self.stack[-1]
 
-
   def sizeStack(self):
-    return len(self.stack)
-
-
-stack = Stack()
-stack.push(1)
-stack.push(2)
-stack.push(3)
-print(stack.sizeStack())
-print("Popped: ", stack.pop())
-print("Popped: ", stack.pop())
-print(stack.sizeStack())
-print("Peek:", stack.peek())
-print(stack.sizeStack())
+    return f"Stack's size: {len(self.stack)}"
+    
+stack1 = Stack()
+stack1.push(1)
+stack1.push(2)
+stack1.push(3)
+stack1.push(4)
+stack1.push(5)
+print(stack1.sizeStack())
+print("Popped: ", stack1.pop())
+print("Popped: ", stack1.pop())
+print(stack1.sizeStack())
+print("Peek:", stack1.peek())
+print(stack1.sizeStack())
