7b931fff85a843563300e1180ad1b9fecf37c226
Quan <QuanAVuong@users.noreply.github.com>
2018-04-25 05:06:12 -0400

    03.1 - update push(), pop(), and showStack() to correctly visualize the operations

--- 03.1-Stacks.py
+++ 03.1-Stacks.py
@@ -5,11 +5,13 @@ class Stack:
   def isEmpty(self):
     return self.stack == []
 
-  def showStack(self):
+  def showStack(self, operation):
     # [print(f"| {self.stack[-i-1]} |") if i < len(self.stack)-1 else print(f"|_{self.stack[-i-1]}_|") for i, v in enumerate(self.stack)]
     for i, v in enumerate(self.stack):
-      if i == 0: 
-        print(f"| {self.stack[-i-1]} | <--")
+      if i == 0 and operation == "push": 
+        print("|___|" if len(self.stack) == 1 else "|   |", f"<-- {self.stack[-i-1]}")
+      elif i == 0 and operation == "pop": 
+        print(f"|   | --> {self.stack[-1]}")
       elif i < len(self.stack)-1:
         print(f"| {self.stack[-i-1]} |")
       else:
@@ -26,15 +28,16 @@ class Stack:
   
   def push(self, data):
     self.stack.append(data)
-    print(f"pushed {data} to the stack: ", f"( list view: {self.stack} )")
-    self.showStack()
+    print(f"pushing {data} to the stack: ", f"( list view: {self.stack} )")
+    self.showStack("push")
 
   def pop(self):
-    # data = self.stack[-1]
-		# del self.stack[-1]
-		# return data
-      
-    return self.stack.pop()
+    data = self.stack[-1]
+    print(f"Popped last item {data} from stack:  (list view: {self.stack} )")
+    self.showStack("pop")
+    del self.stack[-1]
+    return data
+    # return self.stack.pop()
 
   def peek(self):
     return self.stack[-1]
@@ -47,8 +50,9 @@ stack1.push(3)
 stack1.push(4)
 stack1.push(5)
 print(stack1.sizeStack())
-# print("Popped: ", stack1.pop())
-# print("Popped: ", stack1.pop())
-# print(stack1.sizeStack())
+
+stack1.pop()
+stack1.pop()
+print(stack1.sizeStack())
 # print("Peek:", stack1.peek())
 # print(stack1.sizeStack())
