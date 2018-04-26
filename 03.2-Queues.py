eb6624428327a49a087bd6082645d8ed10d0d013
Quan <QuanAVuong@users.noreply.github.com>
2018-04-25 23:49:31 -0400

    03.2 - dequeue(): return and remove item first added

--- 03.2-Queues.py
+++ 03.2-Queues.py
@@ -12,7 +12,10 @@ class Queue:
   def enqueue(self, data):
     self.queue.append(data)
 
-  
+  def dequeue(self):
+    data = self.queue[0]
+    del self.queue[0]
+    return data
 
 queue1 = Queue()
 print("Is the queue empty ?", queue1.isEmpty())
