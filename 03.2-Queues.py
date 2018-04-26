7eb1fa10d52ca1e569e7619c17d8a23a1e51b605
Quan <QuanAVuong@users.noreply.github.com>
2018-04-25 23:36:57 -0400

    03.2 - isEmpty(): check if queue is empty

--- 03.2-Queues.py
+++ 03.2-Queues.py
@@ -2,3 +2,6 @@ class Queue:
   # Implementing class Queue with methods: isEmpty, enqueue, dequeue, peek, sizeQueue, showQueue
   def __init__(self):
     self.queue = []
+  
+  def isEmpty(self):
+    return self.queue == []
